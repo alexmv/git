1e0d61a41db7c0a8602e57556849c4356b1a50cd
Alex Vandiver <alexmv@dropbox.com>
2017-12-16 01:27:15 -0800

    fsmonitor: Make output of test-dump-fsmonitor more concise
    Rather than display one very long line, summarize the contents of that
    line.  The tests do not currently rely on any content except the first
    line ("no fsmonitor" / "fsmonitor last update").

--- t/helper/test-dump-fsmonitor.c
+++ t/helper/test-dump-fsmonitor.c
@@ -3,7 +3,8 @@
 int cmd_main(int ac, const char **av)
 {
 	struct index_state *istate = &the_index;
-	int i;
+	uint64_t now = getnanotime();
+	int i, valid = 0;
 
 	setenv("GIT_FSMONITOR_TEST", "keep", 1);
 	setup_git_directory();
@@ -13,10 +14,21 @@ int cmd_main(int ac, const char **av)
 		printf("no fsmonitor\n");
 		return 0;
 	}
-	printf("fsmonitor last update %"PRIuMAX"\n", (uintmax_t)istate->fsmonitor_last_update);
+
+	printf("fsmonitor last update %"PRIuMAX", (%.2f seconds ago)\n",
+	       (uintmax_t)istate->fsmonitor_last_update,
+	       (now - istate->fsmonitor_last_update)/1.0e9);
 
 	for (i = 0; i < istate->cache_nr; i++)
-		printf((istate->cache[i]->ce_flags & CE_FSMONITOR_VALID) ? "+" : "-");
+		if (istate->cache[i]->ce_flags & CE_FSMONITOR_VALID)
+			valid++;
+
+	printf("  valid:   %d\n", valid);
+	printf("  invalid: %d\n", istate->cache_nr - valid);
+
+	for (i = 0; i < istate->cache_nr; i++)
+		if (!(istate->cache[i]->ce_flags & CE_FSMONITOR_VALID))
+			printf("   - %s\n", istate->cache[i]->name);
 
 	return 0;
 }
